c0182f6443bada9350f6f337d2c9ec5286c4814a
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-29 22:58:03 +0300

    connected the express framework / set up reading directories / created parcels for the header and footer of the application

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+
+// log request
+app.use(morgan("tiny"));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+//app.set('views', path.resolve(__dirname,"views/ejs"));
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
